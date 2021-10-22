c582cf5462c2991b3d99a059820187b2b3f0761f
Scott Hutley <scotthutley1@comcast.net>
2021-10-22 13:44:24 -0700

    Add Description to Treat.cs, Add migrations

--- SweetTreat/Models/Treat.cs
+++ SweetTreat/Models/Treat.cs
@@ -11,6 +11,7 @@ namespace SweetTreat.Models
 
     public int TreatId { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
     public virtual ApplicationUser User { get; set; }
 
     public virtual ICollection<TreatFlavor> JoinEntities { get; }
