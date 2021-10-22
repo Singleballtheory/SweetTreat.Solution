68605fba5800c3438eda6dba642c2b842b1d0334
Scott Hutley <scotthutley1@comcast.net>
2021-10-22 09:56:42 -0700

    Add AppUser cs and SweetTreatContext.cs

---
+++ SweetTreat/Models/SweetTreatContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetTreat.Models
+{
+  public class SweetTreatContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavor { get; set; }
+
+    public SweetTreatContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
